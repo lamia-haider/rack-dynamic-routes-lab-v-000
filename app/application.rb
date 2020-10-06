82e4d274181532c5a6350a0d5cdc718c29ea5eb5
Lamia Haider <lamia.haider@gmail.com>
2020-10-06 22:20:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,16 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+
     if req.path=="/item/"
       resp.write ""
     else
       resp.write "Route not found"
       resp.status = 404
     end
- 
+
     resp.finish
   end
 end
